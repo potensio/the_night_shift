04773212c8ef2cc12e1e27cd8293a9ca486cd5a6
potensio <hanifyaskur@gmail.com>
2024-05-23 21:47:04 +0700

    3rd deployment

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,9 +5,8 @@ const Hero = () => {
     <div className="flex flex-col items-center min-h-8 gap-8 py-32">
       <p className="text-3xl font-semibold">Hi, I’m Hanif.</p>
       <h1 className="text-5xl font-black text-center sm:text-8xl">
-        YOUR NEXT LOW-CODE
-        <br />
-        DEVELOPMENT PARTNER
+        A VISUAL DEVELOPER
+        <br />& DESIGNER
       </h1>
       <button className="flex justify-center text-2xl text-green-600 border-4 rounded-full border-green-600 py-4 px-8">
         BOOK A CALL
