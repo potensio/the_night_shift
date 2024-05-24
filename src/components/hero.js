2ad9762c915dca97f88236e21133951dcabcf9ef
potensio <hanifyaskur@gmail.com>
2024-05-25 04:32:31 +0700

    -

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="flex flex-1 px-5 py-16 sm:px-8 lg:px-20 lg:py-32">
-      <div className="flex flex-1 flex-col gap-4 lg:gap-8">
+      <div className="flex flex-1 flex-col gap-4 lg:gap-4">
         <h1 className="text-5xl font-extrabold max-w-[920px] md:text-6xl lg:text-[80px]">
           Bubble Development, as a Service
         </h1>
@@ -11,8 +11,8 @@ const Hero = () => {
           Bring your idea to life with a responsive and fully functional app,
           delivered at lightning speed, completely asynchronous.
         </p>
-        <button className="w-fit text-xl font-semibold text-green-600 border-[3px] rounded-[18px] border-[#101010] py-3 px-5 main-cta-shadow lg:py-4 lg:px-6 md:text-xl lg:text-2xl">
-          BOOK A CALL
+        <button className="w-fit text-xl font-semibold text-green-600 border-[3px] rounded-[18px] border-[#101010] py-3 px-5 main-cta-shadow mt-3 lg:py-4 lg:px-6 md:text-xl lg:text-2xl">
+          How it works
         </button>
       </div>
     </div>
--- src/components/process.js
+++ src/components/process.js
@@ -2,12 +2,18 @@ import React from "react";
 
 const Process = () => {
   return (
-    <div className="flex flex-1 py-20 px-5 justify-center lg:py-[120px]">
-      <h2 className="text-4xl text-black font-black w-fit text-center sm:text-5xl lg:text-6xl">
-        USUAL PROCESS,
-        <br />
-        EXCEPTIONAL RESULTS.
-      </h2>
+    <div className="flex flex-1 px-5 py-16 sm:px-8 lg:px-20 lg:py-32">
+      <div className="flex flex-1 flex-col items-center gap-6">
+        <h2 className="text-4xl text-black font-black w-fit text-center sm:text-5xl lg:text-6xl">
+          Usual approach,
+          <br />
+          exceptional results.
+        </h2>
+        <p className="text-xl font-semibold text-center max-w-[800px] lg:text-2xl">
+          Bring your idea to life with a responsive and fully functional app,
+          delivered at lightning speed, completely asynchronous.
+        </p>
+      </div>
     </div>
   );
 };
