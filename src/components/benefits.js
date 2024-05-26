6ced20082481d57b8675ad01f656688c0b08ac8c
potensio <hanifyaskur@gmail.com>
2024-05-26 07:13:54 +0700

    deploy

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -3,9 +3,9 @@ import React from "react";
 const Benefit = () => {
   return (
     <div className="py-20 px-5 sm:px-8 lg:py-32 lg:px-20">
-      <div className="flex gap-20">
-        <div className="flex flex-col max-w-[400px] gap-6">
-          <h2 className="text-6xl font-black">
+      <div className="flex flex-col gap-8 lg:flex-row lg:gap-20">
+        <div className="flex flex-col max-w-[360px] gap-6">
+          <h2 className="text-4xl font-black sm:text-5xl lg:text-6xl">
             Subscribe with - <br /> benefits
           </h2>
           <p className="text-xl font-semibold">
@@ -14,8 +14,35 @@ const Benefit = () => {
             blow your mind.
           </p>
         </div>
-        <div className="flex gap-10">
-          <img src="speed-go-fast.svg" className="w-60 h-60" alt="Logo" />
+        <div className="flex gap-4 lg:gap-10">
+          <div className="flex flex-col gap-2 sm:gap-4 lg:gap-6">
+            <img src="speed-go-fast.svg" className="aspect-square" alt="Logo" />
+            <p className="text-l font-black text-center md:text-xl lg:text-2xl">
+              Quick
+              <br />
+              Turnaround
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 sm:gap-4 lg:gap-6">
+            <img
+              src="stack-of-money.svg"
+              className="aspect-square"
+              alt="Logo"
+            />
+            <p className="text-l font-black text-center md:text-xl lg:text-2xl">
+              Flat
+              <br />
+              monthly fee
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 sm:gap-4 lg:gap-6">
+            <img src="bug.svg" className="aspect-square" alt="Logo" />
+            <p className="text-l font-black text-center md:text-xl lg:text-2xl">
+              30 days
+              <br />
+              Support
+            </p>
+          </div>
         </div>
       </div>
     </div>
